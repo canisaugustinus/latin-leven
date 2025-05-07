ae7b7b454b75f7eb3e385026d306ba9f859da829
canisaugustinus <canisaugustinus@gmail.com>
2025-05-06 22:15:37 -0700

    Avoid duplicates when saving latin words. Also, preserve the order of word discovery when parsing raw-wiktextract-data.jsonl.

--- tools/parse_wiktextract.py
+++ tools/parse_wiktextract.py
@@ -17,28 +17,31 @@ class WiktextractParser:
     @classmethod
     def parse_latin_word_list(cls) -> list[str]:
         """ From the raw-wiktextract-data.jsonl data, pick out Latin words. """
-        latin_words = set()
+        latin_words = {}  # use a dict as an ordered set
         file_size = os.path.getsize(cls.WIKTEXTRACT_DATA)
         progress_bar = tqdm(desc="Loading Latin List", total=file_size)
-        with open(cls.WIKTEXTRACT_DATA, 'r', encoding="utf-8") as f_in, \
-                open(cls.LATIN_WORDS, 'w', encoding="utf-8") as f_out:
+        with open(cls.WIKTEXTRACT_DATA, 'r', encoding="utf-8") as f_in:
             f_in_tell_prev = 0
             line = f_in.readline()
             while line:
                 data = json.loads(line)
                 try:
                     if data['lang_code'] == 'la':
-                        word = data['word']
-                        latin_words.add(word)
-                        f_out.write(''.join([word, '\n']))
+                        word = data['word'].strip()
+                        latin_words[word] = None
                 except KeyError:
                     pass
                 f_in_tell = f_in.tell()
                 progress_bar.update(f_in_tell - f_in_tell_prev)
                 f_in_tell_prev = f_in_tell
                 line = f_in.readline()
+
+        latin_words = list(latin_words.keys())
+        with open(cls.LATIN_WORDS, 'w', encoding="utf-8") as f_out:
+            for word in latin_words:
+                f_out.write(word + '\n')
         progress_bar.close()
-        return list(latin_words)
+        return latin_words
 
 
 if __name__ == '__main__':
